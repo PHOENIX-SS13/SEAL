8e1d5ce92f62f204492a18bb60ff4816fc054187
CASEY GUNGEON <bound2theground@gmail.com>
2024-07-02 12:01:02 -0700

    use avalonia

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ##SEAL (SS14 Entity Auto-Loader)
 
-made with Proton Native under the GNU GPLv3 license
+made with Avalonia under the GNU GPLv3 license
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SEAL.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
